bcbca7c41b87f99a893d4790018b2f7a4efec561
IYANU-98 <olorunleke.iyanuloluwa@gmail.com>
2026-08-04 02:32:26 +0100

    Configured Automatic Meta-ordering To Display Past Questions From Newest To Oldest

--- questions/models.py
+++ questions/models.py
@@ -5,9 +5,11 @@ class PastQuestion(models.Model):
     course_code = models.CharField(max_length=20)
     year = models.IntegerField()
     question_text = models.TextField(blank=True, null=True)
-
-    # Double-check that this exact line is here!
     pdf_file = models.FileField(upload_to='past_questions_pdfs/', blank=True, null=True)
+
+    # <-- ADD THIS BRAND NEW CONFIGURATION BLOCK RIGHT HERE:
+    class Meta:
+        ordering = ['-year']  # The minus '-' means descending order (Newest to Oldest)
 
     def __str__(self):
         return f"{self.course_code} ({self.year})"
